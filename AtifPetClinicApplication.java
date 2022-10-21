e7e9ed0ce271f1a942b8d4521d6945892e3e966b
atifameer786 <86449828+atifameer786@users.noreply.github.com>
2022-10-21 17:43:18 +0530

    Add files via upload

---
+++ AtifPetClinicApplication.java
@@ -0,0 +1,13 @@
+package atif.springFramework.atifpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtifPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtifPetClinicApplication.class, args);
+	}
+
+}
